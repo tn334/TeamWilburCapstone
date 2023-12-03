f82801285a7743368c7672d71c50ad9585281603
NathanSeitz <71901625+NathanSeitz@users.noreply.github.com>
2023-12-03 12:53:10 -0700

    create backend sim and a variant version

---
+++ Wilbur/Simulator/simulator.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include "simulator.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+#define OPEN true
+#define CLOSED false
+
+using namespace std;
+
+SimPump::SimPump() : stiffness(0), minimumVal(0), maximumVal(1) {}
+
+SimPump::SimPump(float startingStiffness, float minVal, float maxVal) : 
+                        stiffness(startingStiffness), minimumVal(minVal), maximumVal(maxVal) {}
+
+bool SimPump::setStiffness(float newStiffness) {
+        if ( newStiffness >= minimumVal && newStiffness <= maximumVal ) {
+            Sleep(((rand() % 3) + 1) * 1000);
+            stiffness = newStiffness;
+            return true;
+        }
+        return false;
+    }
+
+float SimPump::getStiffness() {
+        return stiffness;
+    }
+
+string SimPump::percentStiffness() {
+        int percentStiffness = (int)(stiffness * 100);
+        return (to_string(percentStiffness) + "%");
+    }
+
+SimValve::SimValve() : state(CLOSED) {}
+
+SimValve::SimValve(bool startingState) : state(startingState) {}
+
+bool SimValve::setState(bool stateToBe) {
+        if (stateToBe != state) {
+            Sleep(((rand() % 3) + 1) * 1000);
+            state = stateToBe;
+        }
+        return true;
+    }
+
+bool SimValve::getState() {
+        return state;
+    }
+
+string SimValve::stringState() {
+    if (state) {
+        return "open";
+    }
+    else {
+        return "closed";
+    }
+}
+
+Simulator::Simulator() {
+    srand(time(NULL));
+    numValves = 3;
+    pneumaticPump = SimPump();
+    for (int i = 0; i < 3; i++) {
+        valveList.push_back(SimValve());
+    }
+}
+
+Simulator::Simulator(int numValves): numValves(numValves) {
+    srand(time(NULL));
+    pneumaticPump = SimPump();
+    for (int i = 0; i < numValves; i++) {
+        valveList.push_back(SimValve()); 
+    }
+}
+
+bool Simulator::setValve(int valveToSet, bool stateToSet) {
+    if (valveToSet > -1 && valveToSet < valveList.size()) {
+        return valveList.at(valveToSet).setState(stateToSet);
+    }
+    return false;
+}
+
+bool Simulator::setPump(float stiffnessToSet) {
+    return pneumaticPump.setStiffness(stiffnessToSet);
+}
+
+void Simulator::displayState() {
+    cout << "Simulator Component Status:" << endl;
+    cout << "    Pump stiffness: " << pneumaticPump.percentStiffness() << endl;
+    for (int i = 0; i < numValves; i++) {
+        cout << "    Valve " << i << " state: " << valveList.at(i).stringState() << endl;
+    }
+}
---
+++ Wilbur/Simulator/simulator.h
@@ -0,0 +1,64 @@
+#ifndef SIMULATOR
+#define SIMULATOR
+#include <iostream>
+#include <vector>
+using namespace std;
+#define OPEN true
+#define CLOSED false
+
+class SimPump {
+    public:
+
+    SimPump();
+
+    SimPump(float startingStiffness, float minVal, float maxVal);
+
+    bool setStiffness(float newStiffness);
+
+    float getStiffness();
+    string percentStiffness();
+
+    private:
+    float stiffness;
+    float minimumVal;
+    float maximumVal;
+
+};
+
+class SimValve {
+    public:
+
+    SimValve();
+
+    SimValve(bool startingState);
+
+    bool setState(bool stateToBe);
+
+    bool getState();
+
+    string stringState();
+
+    private:
+    bool state;
+};
+
+class Simulator {
+    public:
+
+    Simulator();
+
+    Simulator(int numValves);
+
+    bool setValve(int valveToSet, bool stateToSet);
+
+    bool setPump(float stiffnessToSet);
+
+    void displayState();
+
+    private:
+    SimPump pneumaticPump;
+    vector<SimValve> valveList;
+    int numValves;
+};
+
+#endif
