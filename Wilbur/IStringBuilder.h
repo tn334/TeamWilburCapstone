46e383cbebe344fd97bca7da6614e5de7c6f3990
NathanSeitz <nrs285@nau.edu>
2024-03-07 10:40:49 -0700

    Create interface string builder class. Fixes #30

---
+++ Wilbur/IStringBuilder.h
@@ -0,0 +1,20 @@
+#pragma once
+// Copyright (C) 2016 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#ifndef ISTRINGBUILDER_H
+#define ISTRINGBUILDER_H
+
+// Header files
+#include <QStringList>
+
+class IStringBuilder
+{
+public:
+
+	// Change in front end
+	virtual QString buildString(std::string objectName, int newValue) = 0;
+
+};
+
+#endif // ISTRINGBUILDER_H
