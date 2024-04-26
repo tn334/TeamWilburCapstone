f4a8329efeea18ebb4aed8371ee4f47a245e981e
NathanSeitz <nrs285@nau.edu>
2024-04-25 17:27:56 -0700

    create unit tests for InputDirector

--- WilburTest/TestInputDirector.cpp
+++ WilburTest/TestInputDirector.cpp
@@ -2,15 +2,39 @@
 
 void TestInputDirector::testConstructor()
 {
+    WilburApplicationView window;
+    InputDirectorViewModel* inputDirector = window.getInputDirector();
 
-    InputDirectorViewModel simDirector = new InputDirectorViewModel();
+    QVERIFY(inputDirector->simulatorMode);
 
+    QVERIFY(inputDirector->simOutput != nullptr);
 
+    QVERIFY(typeid(inputDirector->simObject) == typeid(SimulatorModel));
 
 }
 
 void TestInputDirector::testSimFunctions()
 {
-    QString str = "Hello";
-    QCOMPARE(str.toUpper(), QString("HELLO"));
+    WilburApplicationView window;
+    InputDirectorViewModel* inputDirector = window.getInputDirector();
+
+    QVERIFY(inputDirector->handleInput(VALVE1, OPEN, "Switch One"));
+
+    QVERIFY(inputDirector->handleInput(VALVE1, CLOSED, "Switch One"));
+
+    QVERIFY(inputDirector->handleInput(VALVE2, OPEN, "Switch Two"));
+
+    QVERIFY(inputDirector->handleInput(VALVE2, CLOSED, "Switch Two"));
+
+    QVERIFY(inputDirector->handleInput(VALVE3, OPEN, "Switch Three"));
+
+    QVERIFY(inputDirector->handleInput(VALVE3, CLOSED, "Switch Three"));
+
+    QVERIFY(inputDirector->handleInput(PUMP, LOW, "Pump"));
+
+    QVERIFY(inputDirector->handleInput(PUMP, MED, "Pump"));
+
+    QVERIFY(inputDirector->handleInput(PUMP, HIGH, "Pump"));
+
+    QVERIFY(inputDirector->handleInput(PUMP, OFF, "Pump"));
 }
--- WilburTest/TestInputDirector.h
+++ WilburTest/TestInputDirector.h
@@ -3,6 +3,7 @@
 #define TESTINPUTDIRECTOR_H
 
 #include "InputDirectorViewModel.h"
+#include "WilburApplicationView.h"
 #include <QObject>
 #include <QTest>
 
